f69c32cb9c859f9f477b149c8f85bf5c2871323e
stian <stianbekkelund@hotmail.com>
2021-05-22 14:34:45 +0200

    display data and components finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import { BiArchive } from "react-icons/bi";
+import { BiCalendar, BiTrash } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointments from "./components/AddApointment";
+import appointmentList from "./data.json";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        <BiArchive /> Your Appointments
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-5">
+        <BiCalendar className="inline-block text-red-400 align-top" />
+        Your Appointments
       </h1>
+      <AddAppointments />
+      <Search />
+
+      <ul className="divided-y divide-gray-200">
+        {appointmentList.map((appointment) => (
+          <li className="px-3 py-3 flex items-start">
+            <button
+              type="button"
+              className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
+              <BiTrash />
+            </button>
+            <div className="flex-grow">
+              <div className="flex items-center">
+                <span className="flex-none font-medium text-2xl text-blue-500">
+                  {appointment.petName}
+                </span>
+                <span className="flex-grow text-right">
+                  <BiCalendar className=" inline-block align-baseline" />
+                  {appointment.aptDate}
+                </span>
+              </div>
+              <div>
+                <b className="font-bold text-blue-500">Owner: </b>
+                {appointment.ownerName}
+              </div>
+              <div className="leading-tight">{appointment.aptNotes}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
